dd4c68cbad6ec444e109693a2ffdb6117e418f72
Leah Cardoz <leahcardoz@gmail.com>
2023-10-06 16:53:45 -0400

    refactored conditional rendering

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,44 +3,56 @@ import { getMovies } from '../services/fakeMovieService';
 
 const Movies = () => {
 
-  const [data, getData] = useState({
+  const [data, setData] = useState({
     movies: getMovies(),
   })
 
-  const handleDelete = movie => {
-    console.log('deleted movie')
+  const handleDelete = (movie) => {
+    const movies = data.movies.filter(m => m._id !== movie._id)
+    setData({movies})
   }
 
   const movieData = data.movies.map(movie => 
-    <tr key={movie.title} style={{borderBottom: '1px solid #a8a8a9'}}>
-      <td style={{paddingRight: 200, paddingTop: 20, paddingBottom: 20}}>{movie.title}</td>
-      <td style={{paddingRight: 100}}>{movie.genre.name}</td>
-      <td style={{paddingRight: 100}}>{movie.numberInStock}</td>
-      <td style={{paddingRight: 100}}>{movie.dailyRentalRate}</td>
-      <td><button type="button" className="btn btn-danger">Delete</button></td>
+    <tr key={movie._id}>
+      <td>{movie.title}</td>
+      <td>{movie.genre.name}</td>
+      <td>{movie.numberInStock}</td>
+      <td>{movie.dailyRentalRate}</td>
+      <td>
+        <button 
+          className="btn btn-danger btn-sm"
+          onClick={() => handleDelete(movie)}
+        >
+          Delete
+        </button>
+      </td>
     </tr>
   )
 
   return (
-    <>
-      <h4 style={{margin: 20}}>
-        {data.movies.length === 0 ? 'There are no movies in the database.' : `Showing ${data.movies.length} movies in the database.`}
-      </h4>
-      <table>
-        <thead>
-          <tr style={{borderTop: '1px solid #a8a8a9', borderBottom: '2px solid #a8a8a9'}}>
-            <th style={{padding: 20}}>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {movieData}
-        </tbody>
-      </table>
-    </>
+    <div style={{marginTop: 20, marginBottom: 20}}>
+      {data.movies.length === 0 ? 
+        <p>There are no movies in the database. </p>
+      : 
+        <>
+          <p>Showing {data.movies.length} movies in the database.</p>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {movieData}
+            </tbody>
+          </table>
+        </>
+      }
+    </div>
   )
 }
 
