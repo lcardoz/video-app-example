d06f3f92e6f5728001f89f401f23972a2c016514
Leah Cardoz <leahcardoz@gmail.com>
2023-10-12 15:46:41 -0400

    removed state from Genres component and added genres as a property to the state object in Movies component, set defaultProps for Genres component

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,17 +3,19 @@ import Likes from './common/Likes';
 import Pagination from './common/Pagination';
 import Genres from './common/Genres';
 import { getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
 import { paginate } from '../utils/paginate';
 
 const Movies = () => {
 
   const [data, setData] = useState({
     movies: getMovies(),
+    genres: getGenres(),
     currentPage: 1,
     pageSize: 4,
   })
 
-  const {movies, currentPage, pageSize} = data;
+  const {movies, genres, currentPage, pageSize} = data;
   const count = movies.length;
 
   const handleDelete = (movie) => {
@@ -33,6 +35,10 @@ const Movies = () => {
     setData({...data, currentPage: page })
   }
 
+  const handleGenreClick = genre => {
+    console.log(genre)
+  }
+
   const paginatedMovies = paginate(movies, currentPage, pageSize);
 
   return (
@@ -40,52 +46,59 @@ const Movies = () => {
       {count === 0 ? 
         <p>There are no movies in the database. </p>
       : 
-        <>
-          <Genres />
-          <p>Showing {count} movies in the database.</p>
-          <table className='table'>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {paginatedMovies.map(movie => 
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Likes 
-                      liked={movie.liked}
-                      onClick={() => handleLike(movie)} 
-                    />
-                  </td>
-                  <td>
-                    <button 
-                      className="btn btn-danger btn-sm"
-                      onClick={() => handleDelete(movie)}
-                    >
-                      Delete
-                    </button>
-                  </td>
+        <div className="row">
+          <div className="col-3">
+            <Genres 
+              genres={genres} 
+              onGenreClick={handleGenreClick}
+            />
+          </div>
+          <div className="col">
+            <p>Showing {count} movies in the database.</p>
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Genre</th>
+                  <th>Stock</th>
+                  <th>Rate</th>
+                  <th></th>
+                  <th></th>
                 </tr>
-              )}
-            </tbody>
-          </table>
-          <Pagination 
-            itemsCount={count} 
-            pageSize={pageSize} 
-            currentPage={currentPage}
-            onPageChange={handlePageChange} 
-          />
-        </>
+              </thead>
+              <tbody>
+                {paginatedMovies.map(movie => 
+                  <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                      <Likes 
+                        liked={movie.liked}
+                        onClick={() => handleLike(movie)} 
+                      />
+                    </td>
+                    <td>
+                      <button 
+                        className="btn btn-danger btn-sm"
+                        onClick={() => handleDelete(movie)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+            <Pagination 
+              itemsCount={count} 
+              pageSize={pageSize} 
+              currentPage={currentPage}
+              onPageChange={handlePageChange} 
+            />
+          </div>
+        </div>
       }
     </div>
   )
--- src/components/common/Genres.jsx
+++ src/components/common/Genres.jsx
@@ -1,32 +1,28 @@
-import React, { useState } from 'react';
-import { getGenres } from '../../services/fakeGenreService';
+import React from 'react';
 
-const Genres = () => {
-
-  const [genreData, setGenreData] = useState({
-    genres: getGenres(),
-  })
-
-  const {genres} = genreData;
-
+const Genres = ({ genres, onGenreClick }) => {
+  
   return (
     <>
-    <ul className="list-group">
-      <li className="list-group-item">All Genres</li>
-    </ul>
-    {genres.map(genre => (
-    <ul className="list-group">
-      <li 
-        key={genre._id} 
-        className="list-group-item"
-        // onClick={handleGenreClick}
-      >
-        {genre.name}
-      </li>
-    </ul>
-    ))}  
+      <ul className="list-group">
+        <li className="list-group-item">All Genres</li>
+        {genres.map(genre => (
+          <li 
+          key={genre._id} 
+          className="list-group-item"
+          onClick={() => onGenreClick(genre.name)}
+          >
+          {genre.name}
+        </li>
+        ))}  
+      </ul>
     </>
   )
+}
+
+Genres.defaultProps = {
+  textProperty: 'name',
+  valueProperty: '_id'
 }
 
 export default Genres;
